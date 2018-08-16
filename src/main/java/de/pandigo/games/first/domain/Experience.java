59bc135c9f7b9bd3a7e301ae94c942d6c72fa228
Valentin Durst <ValentinDurst@gmail.com>
2018-08-17 00:12:22 +0200

    Added two test domain objects.

---
+++ src/main/java/de/pandigo/games/first/domain/Experience.java
@@ -0,0 +1,14 @@
+package de.pandigo.games.first.domain;
+
+public class Experience {
+
+    private int amount;
+
+    public int getAmount() {
+        return this.amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+}
